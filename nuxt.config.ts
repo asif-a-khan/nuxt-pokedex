02e91c3a6edd050034361730fd1bbf6524820389
Asif <asif14.k@hotmail.com>
2023-03-22 14:18:59 -0400

    Installed utility deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    css: ['vuetify/lib/styles/main.sass'],
+    build: {
+        transpile: ['vuetify'],
+    },
+    modules: ['@pinia/nuxt'],
+    pinia: {
+        autoImports: [
+            // automatically imports `defineStore`
+            'defineStore', // import { defineStore } from 'pinia'
+        ],
+    },
+    ssr: false
 })
